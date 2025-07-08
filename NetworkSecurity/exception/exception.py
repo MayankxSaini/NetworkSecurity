d01b660936a8e6c3a691eedbdbab5a8a9701202b
MayankxSaini <mayankxsaini@gmail.com>
2025-07-08 17:37:49 +0530

    data validation comp

--- NetworkSecurity/exception/exception.py
+++ NetworkSecurity/exception/exception.py
@@ -1,22 +1,33 @@
 import sys
 from NetworkSecurity.logging import logger
+
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_messagge=error_message
-        _,_,exc_tb=error_details.exc_info()
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message
 
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        try:
+            _, _, exc_tb = error_details.exc_info()
+            if exc_tb is not None:
+                self.lineno = exc_tb.tb_lineno
+                self.file_name = exc_tb.tb_frame.f_code.co_filename
+            else:
+                self.lineno = None
+                self.file_name = None
+        except:
+            self.lineno = None
+            self.file_name = None
 
-    def __dtr__(self):
-        return "Error occured in python script name[{0}] line number [{1}] error message [{2}]".format(
-        self.file_name,self.lineno,str(self.error_messagge))
+    def __str__(self):
+        return "Error occurred in Python script [{0}] at line number [{1}] with message: [{2}]".format(
+            self.file_name, self.lineno, str(self.error_message)
+        )
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     try:
         logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
+        a = 1 / 0
+        print("This will not be printed", a)
 
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.entity.config_entity import DataIngestionConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
-
-
+from NetworkSecurity.components.data_validation import DataValidation,DataValidationConfig
+import os,sys
 
 if __name__ =='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("Initiate th data ingestion")
+        logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("data initiation completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation completed") 
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
